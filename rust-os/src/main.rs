786636dfe5125003c8dd77f23d16c4d1ce6ef470
WangHunZi <suguguan@qq.com>
2026-01-19 00:19:23 +0800

    syscall about exit

---
+++ rust-os/src/main.rs
@@ -0,0 +1,25 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+extern "C" fn _start() -> u32 {
+    let code: u32 = 11;
+    let mut ret: u32 = 0;
+    unsafe {
+        core::arch::asm!(
+            "ecall",
+            inlateout("x10") code => ret,
+            in("x11") 0,
+            in("x12") 0,
+            in("x17") 93,
+        );
+    }
+    ret
+}
