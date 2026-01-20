e9ba7e390dc2ffe95712c30f5afdb4e97c1f7c26
WangHunZi <suguguan@qq.com>
2026-01-20 23:48:07 +0800

    implement a syscall wrapper function

--- rust-os/src/main.rs
+++ rust-os/src/main.rs
@@ -1,34 +1,31 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+use core::{arch::asm, panic::PanicInfo};
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+fn syscall(number: usize, arg0: usize, arg1: usize, arg2: usize) -> isize {
+    let mut ret: isize;
+    unsafe {
+        asm!(
+            "ecall",
+            inlateout("x10") arg0 => ret,
+            in("x11") arg1,
+            in("x12") arg2,
+            in("x17") number
+        );
+    };
+    ret
+}
+
 #[no_mangle]
 extern "C" fn _start() {
-    let text: &[u8] = b"Hello, world!\n";
+    let text: &[u8] = b"Hello, RISCV!";
 
-    let code: u32 = 64;
-    let mut ret: u32 = 0;
-    unsafe {
-        core::arch::asm!(
-            "ecall",
-            inlateout("x10") 1 => ret,
-            in("x11") text.as_ptr() as u32,
-            in("x12") text.len() as u32,
-            in("x17") code,
-        );
-
-        core::arch::asm!(
-            "ecall",
-            inlateout("x10") 9 => ret,
-            in("x11") 0,
-            in("x12") 0,
-            in("x17") 93,
-        );
-    }
+    let exit = syscall(64, 1, text.as_ptr() as usize, text.len());
+    syscall(93, exit as usize, 0, 0);
 }
