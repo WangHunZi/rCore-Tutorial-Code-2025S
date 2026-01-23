39fe89d3e8cf22015b630555496cfd3c3799fd39
WangHunZi <suguguan@qq.com>
2026-01-24 00:43:41 +0800

    Hello RISCV!\n

--- rust-os/src/main.rs
+++ rust-os/src/main.rs
@@ -1,31 +1,32 @@
 #![no_std]
 #![no_main]
 
-use core::{arch::asm, panic::PanicInfo};
+use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-fn syscall(number: usize, arg0: usize, arg1: usize, arg2: usize) -> isize {
-    let mut ret: isize;
-    unsafe {
-        asm!(
-            "ecall",
-            inlateout("x10") arg0 => ret,
-            in("x11") arg1,
-            in("x12") arg2,
-            in("x17") number
-        );
-    };
-    ret
-}
-
 #[no_mangle]
 extern "C" fn _start() {
-    let text: &[u8] = b"Hello, RISCV!";
+    unsafe {
+        const TEXT: &[u8] = b"Hello RISCV!\n";
+        core::arch::asm!(
+            "li t1, 0x10000000",
+            "mv t0, a0",
+            "mv t2, a1",
+        "1:",
+            "beqz t2, 2f",
+            "lb t3, 0(t0)",
+            "sb t3, 0(t1)",
 
-    let exit = syscall(64, 1, text.as_ptr() as usize, text.len());
-    syscall(93, exit as usize, 0, 0);
+            "addi t0, t0, 1",
+            "addi t2, t2, -1",
+            "j 1b",
+        "2:",
+            in("a0") TEXT.as_ptr(),
+            in("a1") TEXT.len(),
+        );
+    }
 }
